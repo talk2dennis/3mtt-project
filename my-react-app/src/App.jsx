ed502e9f77a38a9666ef14e1134ee000b0f0a658
talk2dennis <adigwedennis@gmail.com>
2025-04-05 21:01:41 +0100

    hello world

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Hello World</h1>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
